184b7878b9839a57aebdb9aef8eedb005484c42f
Atharvv651 <bhawna.tandon@gmail.com>
2024-08-31 20:19:01 +0530

    second commit

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,11 @@
-print("Hello World")
+word = input("Enter A Word: ")
+
+reversed_word = ""
+
+for letter in word:
+    reversed_word = reversed_word + word[-(word.index(letter)+1)] 
+
+if reversed_word == word:
+    print("This is a palindrome")
+else:
+    print("This is not palindrome")
